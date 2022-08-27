7e6bae21f76f675adde681ccab71bb77e99127dd
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-27 11:16:10 +0500

    Attempt #2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int divider = toBeDivided / 5;
+        float divider = toBeDivided / 5;
         System.out.println(divider);
     }
 }
